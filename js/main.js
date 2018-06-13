d347fddd479ca5a88f62f55c0cf184f96f4f2f40
Vinícius de Oliveira Souza <viniciuso970@gmail.com>
2018-06-13 22:18:16 +0000

    Iniciando carousel

--- js/main.js
+++ js/main.js
@@ -1,13 +1,29 @@
-window.onload=function(){
+window.onload = function () {
     document.getElementById("nav-menu").addEventListener("click", navMenu);
-    //document.getElementById("nav-close").addEventListener("click", navClose);
+    carousel();
 }
 
 function navMenu() {
     var display = document.getElementById("nav-collapse").style.display;
-    if(display == "block") {
+    if (display == "block") {
         document.getElementById("nav-collapse").style.display = "none";
     } else {
         document.getElementById("nav-collapse").style.display = "block";
     }
+}
+
+var indexImg = 0;
+
+function carousel() {
+    var i;
+    var img = document.getElementsByClassName("carousel__img");
+    for (i = 0; i < img.length; i++) {
+        img[i].style.display = "none";
+    }
+    indexImg++;
+    if (indexImg >= img.length) {
+        indexImg = 0;
+    }
+    img[indexImg].style.display = "block";
+    setTimeout(carousel, 1000); // Change image every 2 seconds
 }
